4186ee3a1addb9c441a36e31e901166c0c7e00d5
Amanar Marouane <marouane.amanar07@gmail.com>
2024-11-27 15:14:10 +0100

    Commit

--- script.js
+++ script.js
@@ -1,95 +1,225 @@
-let signUp_button = document.querySelector(".header-nav-signUp").querySelector("button")
+let button = document.querySelectorAll(".button")
 
 function clickEffect() {
-    signUp_button.style.transform = 'scale(0.8)';
-    signUp_button.style.transition = 'all .2s ease-in-out';
-    setTimeout(() => {
-        signUp_button.style.transform = '';
-    }, 200);
+    button.forEach(e => {
+        e.addEventListener("mouseover", () => {
+            e.style.transform = 'scale(0.8)';
+            e.style.transition = 'all .2s ease-in-out';
+            setTimeout(() => {
+                e.style.transform = '';
+            }, 200);
+        })
+    })
 }
 
-let currFormation = '4-3-3'
+let currFormation = document.querySelector("#TeamStrategie")
+let F_T_T = document.querySelector(".F-T-T")
 let cardToMove = document.querySelectorAll(".card")
 let att = document.querySelector(".att").querySelectorAll(".card")
 let mid = document.querySelector(".mid").querySelectorAll(".card")
 let def = document.querySelector(".def").querySelectorAll(".card")
 function Move() {
-    if (currFormation === '4-3-3') {
-        mid[1].style.transform = "translateY(20px)"
-        def[2].style.transform = "translateY(20px)"
-        def[1].style.transform = "translateY(20px)"
-    }
+    currFormation.addEventListener("change", () => {
+        if (currFormation.value === '4-3-3') {
+            mid[1].style.transform = "translateY(20px)"
+            def[2].style.transform = "translateY(20px)"
+            def[1].style.transform = "translateY(20px)"
+            F_T_T.style.display = 'flex'
+        } else {
+            F_T_T.style.display = 'none'
+        }
+    })
 }
 Move()
 
-let cardTemlate = `<div class="card GK">
-                            <img class="backgroung" src="./media/emptyBadge.png" alt="">
+let playersContainer = document.querySelector(".playersDisplay")
+let playersData;
+let statistics;
+async function fetch0() {
+    const response = await fetch("http://localhost:3000/players", {
+        method: 'GET',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+    });
+
+    if (!response.ok) {
+        console.log(`HTTP error! Status: ${response.status}`);
+    }
+
+    playersData = await response.json();
+    playersData.forEach(e => {
+        let position = e.position;
+        let rating = e.rating;
+        let name = e.name;
+        let nationality = e.nationality;
+        let flag = e.flag;
+        let logo = e.logo;
+        let photo = e.photo;
+        let club = e.club;
+        let cardTemplate = `<div class="card ${position}">
+                            <img class="backgroung" src="./media/badge0.png" alt="">
                             <div class="info">
-                                <h3 class="rating">94</h3>
-                                <h3 class="position">CM</h3>
+                                <h3 class="rating">${rating}</h3>
+                                <h3 class="position">${position}</h3>
                             </div>
                             <div class="photo">
-                                <img src="https://cdn.sofifa.net/players/230/621/25_120.png" alt="">
+                                <img src="${photo}" alt="">
                             </div>
-                            <div class="name">Zidane</div>
-                            <div class="statistics">
-                                <div class="PAC"><span>PAC</span><br><span>99</span></div>
-                                <div class="SHO"><span>SHO</span><br><span>99</span></div>
-                                <div class="PAS"><span>PAS</span><br><span>99</span></div>
-                                <div class="DRI"><span>DRI</span><br><span>99</span></div>
-                                <div class="DEF"><span>DEF</span><br><span>99</span></div>
-                                <div class="PHY"><span>PHY</span><br><span>99</span></div>
-                            </div>
+                            <div class="name">${name}</div>
                             <div class="info1">
                                 <div class="flag">
-                                    <img src="https://cdn.sofifa.net/flags/it.png" alt="">
+                                    <img src="${flag}" alt="${nationality}">
                                 </div>
                                 <div class="club">
-                                    <img src="https://cdn.sofifa.net/meta/team/591/120.png" alt="">
+                                    <img src="${logo}" alt="${club}">
                                 </div>
                             </div>
-                            <div class="CPostion">
-                                <p>GK</p>
-                            </div>
                         </div>`
+        if (e.position === "GK") {
+            let div = e.div;
+            let han = e.han;
+            let kic = e.kic;
+            let ref = e.ref;
+            let spe = e.spe;
+            let pos = e.pos;
+            statistics = `<div class="statistics">
+                                <div class="div"><span>div</span><br><span>${div}</span></div>
+                                <div class="han"><span>han</span><br><span>${han}</span></div>
+                                <div class="kic"><span>kic</span><br><span>${kic}</span></div>
+                                <div class="ref"><span>ref</span><br><span>${ref}</span></div>
+                                <div class="spe"><span>spe</span><br><span>${spe}</span></div>
+                                <div class="pos"><span>pos</span><br><span>${pos}</span></div>
+                            </div>`;
+        } else {
+            let pac = e.pac;
+            let sho = e.sho;
+            let pas = e.pas;
+            let dri = e.dri;
+            let Def = e.def;
+            let phy = e.phy
+            statistics = `<div class="statistics">
+                                <div class="pac"><span>pac</span><br><span>${pac}</span></div>
+                                <div class="sho"><span>sho</span><br><span>${sho}</span></div>
+                                <div class="pas"><span>pas</span><br><span>${pas}</span></div>
+                                <div class="dri"><span>dri</span><br><span>${dri}</span></div>
+                                <div class="Def"><span>Def</span><br><span>${Def}</span></div>
+                                <div class="phy"><span>phy</span><br><span>${phy}</span></div>
+                            </div>`;
+        }
+        playersContainer.innerHTML += cardTemplate
+        let addedPlayers = playersContainer.querySelectorAll(".card")
+        addedPlayers[addedPlayers.length - 1].innerHTML += statistics;
+    })
 
-// let playersData;
+    global()
+}
+fetch0();
 
-// async function global() {
-//     // await fetch0();
-//     await fetch1();
-// }
+let AttCards = [];
+let MidCards = [];
+let DefCards = [];
+let KepCards = [];
 
-// async function fetch0() {
-//     const response = await fetch("./players.json", {
-//         method: 'GET',
-//         headers: {
-//             "Content-Type": 'application/json'
-//         },
-//     });
+function global() {
+    let addedPlayers = playersContainer.querySelectorAll(".card")
+    let cards = document.querySelectorAll(".shadow")
+    function playerAppeand() {
+        cards.forEach(e => {
+            e.addEventListener("click", () => {
+                console.log("Click To Add");
+                playersContainer.innerHTML = ''
+                let role = e.parentElement.className;
+                if (role === 'att') {
+                    console.log("Att");
+                    AttCards.forEach(e => playersContainer.appendChild(e));
+                }
+                else if (role === 'mid') {
+                    console.log("mid");
+                    MidCards.forEach(e => playersContainer.appendChild(e));
+                }
+                else if (role === "def") {
+                    console.log("def");
+                    DefCards.forEach(e => playersContainer.appendChild(e));
+                }
+                else {
+                    console.log("Kep");
+                    KepCards.forEach(e => playersContainer.appendChild(e));
+                }
+            })
+        })
+    }
 
-//     if (!response.ok) {
-//         console.log(`HTTP error! Status: ${response.status}`);
-//     }
+    function Filter() {
+        addedPlayers.forEach(e => {
+            let position = e.className;
 
-//     playersData = await response.json();
-//     console.log("Fetched playersData:", playersData);
+            if (!position.includes("occupied")) {
+                if (position.includes("RW") || position.includes("LW") || position.includes("ST")) {
+                    AttCards.push(e)
+                }
+                if (position.includes("CM") || position.includes("LM") || position.includes("RM")) {
+                    MidCards.push(e)
+                }
+                if (position.includes("CB") || position.includes("LB") || position.includes("RB")) {
+                    DefCards.push(e)
+                }
+                if (position.includes("GK")) {
+                    KepCards.push(e)
+                }
+            } else {
+                console.log(`${position} didn't pass!!`);
 
-// }
+            }
+        })
+    }
+    Filter()
+    playerAppeand()
 
-// async function fetch1() {
-//     playersData = {};
-//     const response = await fetch("./players.json", {
-//         method: 'POST',
-//         headers: {
-//             "Content-Type": 'application/json'
-//         },
-//         body: JSON.stringify(playersData)
-//     });
+    let formulaireCards = document.querySelectorAll(".stadiumCard");
+    let Classes = formulaireCards[0].className;
+    let classToAdd = Classes.split(' ');
+    function mod() {
+        formulaireCards.forEach(e => {
+            e.addEventListener("click", () => {
+                // console.log(e);
+                // console.log(this);
 
-//     if (!response.ok) {
-//         console.log(`HTTP error!`);
-//     }
-// }
+                // formulaireCards.forEach(j => {
+                //     j.style.scale = 1;
+                // })
+                // e.style.scale = 1.2
+                // e.style.transition = 'all .3s ease'
+                // flag = true;
+                let playersContainer = document.querySelector(".playersDisplay").querySelectorAll(".card");
+                playersContainer.forEach(i => {
+                    console.log(1);
 
-// global();
+                    i.addEventListener("click", () => {
+                        console.log(e);
+
+                        
+                        e.innerHTML = i.innerHTML;
+                        // e.className = '';
+                        // classToAdd.forEach(Class => e.classList.add(Class))
+                        // i.classList.add("occupied")
+                        //mod()
+                        // Filter()
+
+                    }, { once: true })
+
+                })
+            }, { once: true })
+            // console.log(e)
+            // playersContainer.querySelectorAll(".card").forEach(e => {
+            //     if (e.className.includes("occupied")) {
+            //         e.outerHTML = ''
+            //     }
+            // })
+        })
+    }
+    mod()
+    function handleSwitch(output, input) {
+        output.innerHTML = input.innerHTML
+    }
+}
